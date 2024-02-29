9821e52953a58203f1204fd81ba7e3876ce29e45
Erwan KERFOURN <erwankerfourn@icloud.com>
2024-02-29 11:08:49 -0800

    correction 001

--- test.py
+++ test.py
@@ -1,41 +1,12 @@
+from diffusers import DiffusionPipeline
 import torch
-from diffusers import StableCascadeDecoderPipeline, StableCascadePriorPipeline
 
-device = "cuda"
-num_images_per_prompt = 2
+pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+pipe.to("cuda")
 
-# Loading the prior pipeline
-prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
+# if using torch < 2.0
+# pipe.enable_xformers_memory_efficient_attention()
 
-# Loading the decoder pipeline with additional parameters to ignore mismatched sizes
-decoder = StableCascadeDecoderPipeline.from_pretrained(
-    "stabilityai/stable-cascade",
-    torch_dtype=torch.float16,
-    ignore_mismatched_sizes=True,  # Add this line to ignore the mismatched sizes error
-).to(device)
+prompt = "An astronaut riding a green horse"
 
-prompt = "Anthropomorphic cat dressed as a pilot"
-negative_prompt = ""
-
-# Generating image embeddings using the prior
-prior_output = prior(
-    prompt=prompt,
-    height=1024,
-    width=1024,
-    negative_prompt=negative_prompt,
-    guidance_scale=4.0,
-    num_images_per_prompt=num_images_per_prompt,
-    num_inference_steps=20
-)
-
-# Decoding the image embeddings into images
-decoder_output = decoder(
-    image_embeddings=prior_output.image_embeddings.half(),  # Ensure embeddings are in the correct dtype
-    prompt=prompt,
-    negative_prompt=negative_prompt,
-    guidance_scale=0.0,
-    output_type="pil",
-    num_inference_steps=10
-).images
-
-# Now, decoder_output is a list with your PIL images
+images = pipe(prompt=prompt).images[0]
